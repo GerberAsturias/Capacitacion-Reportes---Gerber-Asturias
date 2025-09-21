ead165a274128321de439d63439f8416d1ae15d7
GerberAsturias <149099484+GerberAsturias@users.noreply.github.com>
2025-09-20 23:06:59 -0600

    Add files via upload

---
+++ Reportes1/Reportes1/Reporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reportes1
+{
+    public partial class Reporte : Form
+    {
+        public Reporte()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
